bb991ffe7e885f545bd2cf5531f200153f4116ac
JothikaGit <jothika1050@gmail.com>
2024-11-16 20:47:23 +0530

    updated greedent project

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./src/**/*.{html,js}"], // Scans all .html and .js files in src folder
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
